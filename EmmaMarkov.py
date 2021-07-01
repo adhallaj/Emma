582bc0cc6588112c280fd8036fbda9d2d24981b6
Adam <58492241+adhallaj@users.noreply.github.com>
2021-07-01 14:05:00 -0500

    Update EmmaMarkov.py

--- EmmaMarkov.py
+++ EmmaMarkov.py
@@ -1,5 +1,6 @@
 transitions = ['H', 'N', 'N', 'S', 'S', 'H', 'H', 'H', 'S', 'S', 'N', 'S', 'H', 'N', 'H', 'S', 'H', 'H', 'S', 'S', 'H', 'N', 'H', 'N', 'S', 'S', 'N', 'N', 'N', 'H', 'S', 'H', 'S', 'N', 'H', 'N', 'S', 'S', 'N', 'N', 'S', 'S', 'N', 'S', 'N', 'H', 'N', 'H', 'N', 'H', 'S', 'N', 'H', 'N', 'H', 'S', 'H', 'H', 'N', 'N', 'N', 'S', 'H', 'H', 'H', 'N', 'N', 'H', 'H', 'N', 'H', 'N', 'N', 'S', 'S', 'S', 'N', 'N', 'N', 'N', 'H', 'N', 'N', 'H', 'H', 'S', 'H', 'S', 'S', 'H', 'N', 'S', 'N', 'S', 'S', 'S', 'S', 'H', 'H', 'N', 'S', 'S', 'H', 'S', 'H', 'H', 'S', 'H', 'N', 'S', 'S', 'S', 'H', 'H', 'N', 'S', 'N', 'N', 'S', 'H', 'H', 'S', 'H', 'H', 'N', 'H', 'H', 'H', 'H', 'N', 'S', 'S', 'N', 'N', 'N', 'S', 'N', 'H', 'N', 'S', 'S', 'H', 'S', 'H', 'N', 'H', 'H', 'N', 'S', 'H', 'N', 'S', 'S', 'S', 'S', 'H', 'N', 'H', 'H', 'S', 'N', 'S', 'S', 'S', 'S', 'N', 'H', 'H', 'S', 'S', 'S', 'H', 'N', 'N', 'N', 'N', 'N', 'S', 'H', 'S', 'N', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'S', 'N', 'S', 'N', 'N', 'H', 'N', 'N', 'H', 'H', 'H', 'H', 'H', 'H', 'N', 'H', 'N', 'H', 'H', 'S', 'N', 'N', 'N', 'N', 'H', 'N', 'H', 'H', 'N', 'S', 'S', 'N', 'H', 'H', 'S', 'S', 'S', 'N', 'N', 'H', 'N', 'N', 'H', 'H', 'N', 'S', 'S', 'N', 'H', 'H', 'S', 'S', 'S', 'N', 'N', 'S', 'S', 'H', 'N', 'H', 'H', 'H', 'H', 'H', 'N', 'N', 'N', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'S', 'S', 'S', 'H', 'S', 'S', 'N', 'S', 'S', 'S', 'S', 'S', 'H', 'H', 'N', 'N', 'H', 'H', 'H', 'S', 'H', 'N', 'N', 'H', 'S', 'H', 'H', 'N', 'N', 'N', 'N', 'H', 'H', 'N', 'N', 'N', 'H', 'H', 'N', 'N', 'N', 'H', 'N', 'H', 'N', 'S', 'N', 'N', 'N', 'H', 'H', 'H', 'H', 'N', 'H', 'H', 'N', 'H', 'H', 'H', 'H', 'N', 'N', 'S', 'H', 'N', 'H', 'N', 'H', 'N', 'N', 'N', 'H', 'H', 'H', 'H', 'S', 'H', 'H', 'H', 'N', 'S', 'S', 'H', 'H', 'H', 'H', 'H', 'N', 'N', 'S', 'H', 'H', 'H', 'H', 'H', 'H', 'N', 'H']
-print(transitions)
+
+#make a new list that is compatible with rank and order functions
 
 abctransitions = []
 
@@ -12,8 +13,6 @@ for state in transitions:
         abctransitions.append("C")
     continue
 
-
-print(abctransitions)
 
 def rank(c):
     return ord(c) - ord('A')
