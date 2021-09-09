cfd5a5d69d3d5a3f7cbfa00ca02a6a4367be94ef
Adam <58492241+adhallaj@users.noreply.github.com>
2021-09-09 18:04:13 -0400

    Update EmmaAlive.py

--- EmmaAlive.py
+++ EmmaAlive.py
@@ -1,3 +1,5 @@
+#set up cron to run this script once a day at midnight so it doesn't have to run 24/7
+
 import time
 
 #Probabilities of change between happy (H) neutral (N) and sad (S).
@@ -15,8 +17,12 @@ moodlist = []
 daylist= []
 day = 0
 
+##scrapped this function
 #feel function should occur every day (every 86400 seconds)
-def feel():
-  time.sleep(86400)
-  day += 1
+#def feel():
+#  time.sleep(86400)
+#  day += 1
+
+#every time this script is run have it use the last state (the Nth item in moodlist) as the start state, calculate the new state, and append the new state to moodlist
+
   
